bcb70e76c552bc92b40014d69a53a7f099074909
DanielFl0 <MachoAlph@outlook.com>
2019-06-20 00:16:44 -0500

    Added more chapter 3 exercises

---
+++ Exercise3-9.py
@@ -0,0 +1,2 @@
+#3-9 Dinner guests: Working with one of the programs from exercises 3-4 through 3-7 (page 46), use len() to print a message indicating the number of people you are inviting to dinner.
+
