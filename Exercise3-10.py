f1d490b8084e6d0c32d3ddc2380a008225eb4ff1
DanielFl0 <MachoAlph@outlook.com>
2019-06-23 18:52:07 -0500

    Finished with chapter 3 exercises

--- Exercise3-10.py
+++ Exercise3-10.py
@@ -1,1 +1,36 @@
 #3-10 Every function: Think of something you could store in a list. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you'd like. Write a program that creates a list containing these items and then uses each function introduced in this chapter t least once.
+
+programming_languages = ['Python', 'Java', 'C++', 'C#', 'C']
+
+#Lets start by using the index in our list
+print("My 2 favorite programming languages are: " + programming_languages[0] + " and " + programming_languages[2])
+
+#In the next line, i'm going to edit Java for Ruby
+programming_languages[1] = 'Ruby'
+print(programming_languages)
+
+#Next I will add 3 programming languages using insert() and append()
+programming_languages.insert(2, 'Java')
+print(programming_languages)
+programming_languages.insert(3, 'JavaScript')
+print(programming_languages)
+programming_languages.append('Lua')
+print(programming_languages)
+
+#Next I will remove 3 programming languages using pop(), remove(), and del
+programming_languages.pop()
+print(programming_languages)
+programming_languages.remove('JavaScript')
+print(programming_languages)
+del programming_languages[1]
+print(programming_languages)
+
+#Next I will sort my list with the methods sorted(), sort() and reverse()
+print(sorted(programming_languages))
+print(sorted(programming_languages, reverse=True))
+programming_languages.sort()
+print(programming_languages)
+programming_languages.sort(reverse=True)
+print(programming_languages)
+programming_languages.reverse()
+print(programming_languages)
--- Exercise3-9.py
+++ Exercise3-9.py
@@ -1,2 +1,50 @@
 #3-9 Dinner guests: Working with one of the programs from exercises 3-4 through 3-7 (page 46), use len() to print a message indicating the number of people you are inviting to dinner.
 
+guest_list = ['Samuel', 'Daniel', 'Jose']
+print("Hello " + guest_list[0] + " you're invited to my dinner!")
+print("Hello " + guest_list[1] + " you're invited to my dinner!")
+print("Hello " + guest_list[2] + " you're invited to my dinner!")
+
+print(guest_list[1] + " isn't able to come to the dinner")
+guest_list[1] = 'Sergio'
+
+print("Hello " + guest_list[0] + " you're invited to my dinner!")
+print("Hello " + guest_list[1] + " you're invited to my dinner!")
+print("Hello " + guest_list[2] + " you're invited to my dinner!")
+
+print("I've found a larger dinner table, which means I can invite more people!")
+
+guest_list.insert(0, 'Diego')
+
+guest_list.insert(2, 'Ernesto')
+
+guest_list.append('Luis')
+
+print("Hello " + guest_list[0] + " you're invited to my dinner!")
+print("Hello " + guest_list[1] + " you're invited to my dinner!")
+print("Hello " + guest_list[2] + " you're invited to my dinner!")
+print("Hello " + guest_list[3] + " you're invited to my dinner!")
+print("Hello " + guest_list[4] + " you're invited to my dinner!")
+print("Hello " + guest_list[5] + " you're invited to my dinner!")
+
+print("I'm sorry, the bigger dinner table ins't available, which means I can only invite 2 persons")
+
+uninvited_guest = guest_list.pop()
+print("Hello " + uninvited_guest + " the dinner is cancelled")
+uninvited_guest = guest_list.pop()
+print("Hello " + uninvited_guest + " the dinner is cancelled")
+uninvited_guest = guest_list.pop()
+print("Hello " + uninvited_guest + " the dinner is cancelled")
+uninvited_guest = guest_list.pop()
+print("Hello " + uninvited_guest + " the dinner is cancelled")
+
+print("Hello " + guest_list[0] + " you're invited to my dinner!")
+print("Hello " + guest_list[1] + " you're invited to my dinner!")
+
+del guest_list[1]
+del guest_list[0]
+
+print(guest_list)
+
+#Printing length of guest_list to find out how many people are invited to my dinner.
+print("The total number of guests is: " + str(len(guest_list)))
