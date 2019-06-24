5dd3b7c92beae79bc3e5d750667873690a6b5576
DanielFl0 <MachoAlph@outlook.com>
2019-06-23 19:08:00 -0500

    Added exercise 3-11

---
+++ exercise3-11.py
@@ -0,0 +1,6 @@
+#3-1 Intentional error: If you haven't recieved an index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+programming_languages = ['Python', 'Java', 'C#', 'C++', 'C']
+
+#Intentional error: I will try to print the item in the position 5, this will return a traceback.
+print(programming_languages[5])
