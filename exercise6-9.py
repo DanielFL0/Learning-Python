d7d8d6c8431fb5fb5f62e5a15b5fed1342df43ee
DanielFl0 <MachoAlph@outlook.com>
2019-06-28 20:56:24 -0500

    Added exercise6-9.py

---
+++ exercise6-9.py
@@ -0,0 +1,11 @@
+#6-9 Favorite places: Make a dictionary called favorite_places. Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. To make this exercise a bit more interesting, ask some friends to name a few of their favorite places. Loop through the dictionary, and print each person's name and their favorite places.
+
+favorite_places = {
+    'daniel': ['canada', 'sweden', 'japan'],
+    'sergio': ['japan', 'canada'],
+    'jose': ['denmark', 'sweden', 'europe']
+    }
+for person, places in favorite_places.items():
+    print(person.title() + "'s favorite places are: ")
+    for place in places:
+        print("\t" + place.title())
