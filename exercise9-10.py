d765474c721d8945c27d16daee1445d9d9d631fa
DanielFl0 <MachoAlph@outlook.com>
2019-07-10 23:38:34 -0500

    Added exercise9-10.py, exercise9-11.py and exercise9-12.py

---
+++ exercise9-10.py
@@ -0,0 +1,5 @@
+#9-10 Imported restaurant: Using your latest Restaurant class, store it in a module.
+#Make a separate file that imports Restaurant.
+#Make a Restaurant instance, and call one of Restaurant's methods to show that the import statement is working properly.
+
+
