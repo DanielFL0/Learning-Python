eef48b157f8f3d48931919d5eee910f77ce5ddd2
DanielFl0 <machoalph@outlook.com>
2019-07-14 21:04:20 -0500

    Modified exercise9-11.py and added module admin.py

---
+++ admin.py
@@ -0,0 +1,60 @@
+#Module made from exercise9-8.py
+
+class User():
+    """A class that represents a user"""
+
+    def __init__(self, first_name, last_name, user_name, age):
+        """Initialize attributes to describe a user"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_name = user_name
+        self.age = age
+        self.login_attempts = 0
+
+    def describe_user(self):
+        """Describes user's information"""
+        print("First name: "
+                + self.fisrt_name.title()
+                + "\nLast name: "
+                + self.user_name
+                + "\nAge: "
+                + str(self.age)
+                + "\nLogin attempts: "
+                + str(self.login_attempts))
+
+    def greet_user(self):
+        """Greets the user with a simple message and it's username"""
+        print("Hello, " + self.user_name + "!\n")
+
+    def increment_login_attempts(self):
+        """Increments user's login attempts"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Resets user's login attempts"""
+        self.login_attempts = 0
+
+class Admin(User):
+    """A class that represents an admin"""
+
+    def __init__(self, first_name, last_name, user_name, age):
+        """
+        Initialize user's attributes
+        And added attribute privileges
+        """
+        super().__init__( first_name, last_name, user_name, age)
+        self.privileges = Privileges()
+
+class Privileges():
+    """A class that represents admin's preivileges"""
+
+    def __init__(self):
+        """Initialize attributes to describe admin's privileges"""
+        self.privileges = ['can add post', 'can delete post', 'can ban user']
+
+    def show_privileges(self):
+        """A method that displays admin's privileges"""
+
+        for privilege in self.privileges:
+            print(privilege)
+
--- exercise9-11.py
+++ exercise9-11.py
@@ -2,3 +2,9 @@
 #Store the classes User, Privileges, and Admin in one module.
 #Create a separate file, make an Admin instance, and call show_privileges() to show that everything is working correctly.
 
+from admin import User
+from admin import Admin
+from admin import Privileges
+
+page_admin = Admin('daniel', 'saldana', 'dsal', 20)
+page_admin.privileges.show_privileges()
