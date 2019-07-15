fd358b8f903260110d2315d7fdcdcf98fa009800
DanielFl0 <machoalph@outlook.com>
2019-07-14 20:46:22 -0500

    Modified exercise9-10.py and added the module restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,48 @@
+#Making the Restaurant class from exercise9-6.py a module.
+
+class Restaurant():
+    """A class that represents a restaurant"""
+
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initialize attributes to describe a restaurant"""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+
+    def describe_restaurant(self):
+        """Displays restaurant information"""
+        print("Restaurant's name: " 
+                + self.restaurant_name.title()
+                + "\nCuisine type: "
+                + self.cuisine_type.title()
+                + "\nNumber served: "
+                + str(self.number_served))
+
+    def open_restaurant(self):
+        """Displays a message that the restaurant is now open"""
+        print(self.restaurant_name.title() + " is now open")
+
+    def set_number_served(self, number_served):
+        """Set the number served to a given value"""
+        self.number_served = number_served
+    
+    def increment_number_served(self, number):
+        """Increment the number served to a given value"""
+        self.number_served += number
+
+class IceCreamShop(Restaurant):
+    """A class that represents an ice cream shop"""
+    def __init__(self, restaurant_name, cuisine_type):
+        """
+        Initialize attributes of class Restaurant
+        And adding the attribute flavors
+        """
+        super().__init__(restaurant_name, cuisine_type)
+        self.flavors = ['chocolate', 'vanilla', 'strawberry']
+
+    def show_flavors(self):
+        """A simple method that displays the list of flavors"""
+        for flavor in self.flavors:
+            print(flavor)
+
+
