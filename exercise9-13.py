2c115768aad518685a1256fa0e7af72563e1a425
DanielFl0 <machoalph@outlook.com>
2019-07-15 22:45:50 -0500

    Added exercise9-13.py instructions

---
+++ exercise9-13.py
@@ -0,0 +1,5 @@
+#9-13: OrderedDict Rewrite: Start with exercise6-4.py (page 108), where you
+#used a standard dictionary to represent a glossary.
+#Rewrite the program using the OrderedDict class and make sure the order of the
+#output matches the order in which key-value pairs were added to the dictionary
+
