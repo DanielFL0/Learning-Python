287b228488af875d37f63c5e1386a796a2217023
DanielFl0 <machoalph@outlook.com>
2019-07-31 19:36:51 -0500

    Finished exercise10-1.py

--- exercise10-1.py
+++ exercise10-1.py
@@ -4,7 +4,25 @@
 #Save the file as learning_python.txt in the same directory as your exercise
 #from this chapter. Write a program that reads the file and prints what you
 #wrote three times. Print the contents once by reading in the entire file,
-#once by looping over the file object, and once by storing the lins in a list
+#once by looping over the file object, and once by storing the lines in a list
 #and then working with them outside the with block.
 
+#Store the filename in a variable
+file = 'learning_python.txt'
 
+with open(file) as file_object:
+
+    contents = file_object.read()
+    print(contents.strip())
+
+with open(file) as file_object:
+    for line in file_object:
+        print(line.strip())
+
+with open(file) as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        print(line.strip())
+
+for line in lines:
+    print(line.strip())
